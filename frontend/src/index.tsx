e4938b9846a6410b450b31548ac64ab02438b8bc
Halazv2 <halaziani2003@gnail.com>
2023-03-22 22:12:38 +0000

    sf hada li ankhdm bih

---
+++ frontend/src/index.tsx
@@ -0,0 +1,21 @@
+import ReactDOM from 'react-dom';
+import { HelmetProvider } from 'react-helmet-async';
+import { BrowserRouter } from 'react-router-dom';
+
+import 'nprogress/nprogress.css';
+import App from '@/App';
+import { SidebarProvider } from '@/contexts/SidebarContext';
+import * as serviceWorker from '@/serviceWorker';
+
+ReactDOM.render(
+  <HelmetProvider>
+    <SidebarProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </SidebarProvider>
+  </HelmetProvider>,
+  document.getElementById('root')
+);
+
+serviceWorker.unregister();
