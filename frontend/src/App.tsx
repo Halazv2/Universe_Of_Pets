fc50172338b489d07ecf0b8900c2dcab4c46bec4
Halazv2 <halaziani2003@gmail.com>
2023-04-02 10:38:33 +0000

    create nav bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,29 @@
-import React from "react";
-
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+import { Box } from '@mui/system';
+import React, { useMemo } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './screens/navbar';
+import { themeSettings } from './theme';
 
 function App() {
-  return <div className="app"></div>;
+  const theme = useMemo(() => createTheme(themeSettings), []);
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/predictions" element={<h1>Predictions</h1>} />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
